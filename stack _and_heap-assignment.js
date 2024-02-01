3d3962c3dc87d863db5a04a33011b71affdd2674
nidhiwhocodes <nidhiponnalwar13@gmail.com>
2024-02-01 12:51:22 +0530

    commit a stack and heap code

---
+++ stack _and_heap-assignment.js
@@ -0,0 +1,18 @@
+let arr=["hulk","ironman","black-widow"]
+
+arr[arr.length-1]="thor"
+
+// console.log(arr)
+output: [ 'hulk', 'ironman', 'thor' ]
+
+let str='shaktiman'
+
+let upper=str.toUpperCase()
+
+let lower=str.toLowerCase()
+
+str[7]="e"
+
+// console.log(str)
+output: shaktiman
+
